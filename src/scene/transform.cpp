bfc95b7687ff77194a1e043b4734c9050ca490dd
yooseungjun <hongildong6987@gmail.com>
2024-09-10 18:15:50 +0900

    정보 : 함수이름을 이상하게하면 더 헷갈린다.

--- src/scene/transform.cpp
+++ src/scene/transform.cpp
@@ -1,5 +1,6 @@
 
 #include "transform.h"
+#include <memory>
 
 Mat4 Transform::local_to_parent() const {
   return Mat4::translate(translation) * rotation.to_mat() * Mat4::scale(scale);
@@ -14,14 +15,22 @@ Mat4 Transform::local_to_world() const {
   // A1T1: local_to_world
   // don't use Mat4::inverse() in your code.
 
-  return Mat4::I; //<-- wrong, but here so code will compile
+  std::shared_ptr<Transform> pParentTransform = parent.lock();
+  return pParentTransform
+             ? pParentTransform->local_to_world() * local_to_parent()
+             : Mat4::translate(translation) * rotation.to_mat() *
+                   Mat4::scale(scale);
 }
 
 Mat4 Transform::world_to_local() const {
   // A1T1: world_to_local
   // don't use Mat4::inverse() in your code.
 
-  return Mat4::I; //<-- wrong, but here so code will compile
+  std::shared_ptr<Transform> pParentTransform = parent.lock();
+  return pParentTransform
+             ? parent_to_local() * pParentTransform->world_to_local()
+             : Mat4::scale(1.f / scale) * rotation.inverse().to_mat() *
+                   Mat4::translate(-translation);
 }
 
 bool operator!=(const Transform &a, const Transform &b) {
